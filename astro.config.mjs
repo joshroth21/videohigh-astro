ba8f6f589b2e0997d7a227bca7b7f1d5c430e950
Josh Roth <josh@joshroth.me>
2023-09-05 11:10:37 -0700

    Fix Vercel site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    functionPerRoute: false,
+  }),
 });
